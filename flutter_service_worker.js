db5f98eed670c1b47b4404b9f8b56c676a193c25
keinthmac <keinthmac@yahoo.co.uk>
2021-05-01 15:16:54 +0400

    update on saving transaction

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,9 +14,9 @@ const RESOURCES = {
 "favicon.png": "d8827788ce85114af79017d6f7c03630",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "597451c0e8f54d15a438490dd3f11f38",
-"/": "597451c0e8f54d15a438490dd3f11f38",
-"main.dart.js": "9c222d09e1b258efb5992c90c7465329",
+"index.html": "0c6a3609c03f42158b60efb1197b5953",
+"/": "0c6a3609c03f42158b60efb1197b5953",
+"main.dart.js": "29991304eff06dc3a00b2b0a6fbf9dc5",
 "manifest.json": "6e06320119f240404503be8f4aa5c8a7",
 "version.json": "71d7066c3afcc3a31fb85915b3b9f22a"
 };
