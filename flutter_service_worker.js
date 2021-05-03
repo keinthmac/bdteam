950966bcf739705d901fa4430ae41fa8d6ebfc01
keinthmac <keinthmac@yahoo.co.uk>
2021-05-04 02:25:35 +0400

    updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "favicon.png": "d8827788ce85114af79017d6f7c03630",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "98855ff4f9ecdb29bbc611d0d3823e6f",
-"/": "98855ff4f9ecdb29bbc611d0d3823e6f",
+"index.html": "5b879e708bcd20efcc03b9a2dc8b38db",
+"/": "5b879e708bcd20efcc03b9a2dc8b38db",
 "main.dart.js": "4446c6b2f3128d3649fd5262d7eed20a",
 "manifest.json": "6e06320119f240404503be8f4aa5c8a7",
 "version.json": "71d7066c3afcc3a31fb85915b3b9f22a"
