8832d35d1ea46e598e4dfe03950e02653c6344ce
Kenneth Salatan <kenneth.salatan@bluemena.com>
2021-12-08 01:55:47 -0800

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,9 +12,9 @@ const RESOURCES = {
 "favicon.png": "d8827788ce85114af79017d6f7c03630",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "56e694f59be60ea07bcc99d1aa90fc53",
-"/": "56e694f59be60ea07bcc99d1aa90fc53",
-"main.dart.js": "4e02bf0e0b060571db399db8cd50048d",
+"index.html": "2de7349baef39be6771021b63d7a1296",
+"/": "2de7349baef39be6771021b63d7a1296",
+"main.dart.js": "4b133502fdab4f7e52a09b48d6fd9dca",
 "manifest.json": "6e06320119f240404503be8f4aa5c8a7",
 "version.json": "f42963cc2e10ee03a2e846e73e172854"
 };
