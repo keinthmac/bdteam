adb07aab9c86914f6b9e26189866015f3625d2f7
Kenneth Salatan <kenneth.salatan@blueworld.ai>
2022-09-04 01:26:36 +0400

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "b82e85dc1281ee730f16a90f8bdd07ce",
-"/": "b82e85dc1281ee730f16a90f8bdd07ce",
-"main.dart.js": "723523a2a168f1502566ad8d3abd016d",
+"index.html": "13031036891c3b3015b7b70e906504ce",
+"/": "13031036891c3b3015b7b70e906504ce",
+"main.dart.js": "e25ed8afb82a88820c988e5b752ae80e",
 "manifest.json": "6e06320119f240404503be8f4aa5c8a7",
 "version.json": "f42963cc2e10ee03a2e846e73e172854"
 };
